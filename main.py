f7fa7061b08e4a4f4f39017d190a5573a023594d
Dtlama <serebryakovss1901@gmail.com>
2022-04-23 13:51:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+import datetime
+
+import collections
+
+import pandas
+
+
+dicts = pandas.read_excel('wine3.xlsx', sheet_name='Лист1',
+                          usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'], na_values=['nan'],
+                          keep_default_na=False)
+excel_data_df = dicts.to_dict(orient='records')
+
+wines = collections.defaultdict(list)
+for wine in excel_data_df:
+    wines[wine["Категория"]].append(wine)
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+event_1 = datetime.datetime.now().year
+year = event_1 - 1920
+
+rendered_page = template.render(
+    today_year=year,
+    wines=wines
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(('127.0.0.1', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
